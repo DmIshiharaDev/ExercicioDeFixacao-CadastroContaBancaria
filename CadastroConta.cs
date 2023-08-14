e2f7e2845a8ac671af111acd00a3bd6ed489a18e
DmIshiharaDev <139144547+DmIshiharaDev@users.noreply.github.com>
2023-08-14 14:00:42 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroConta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioDeFixacao_CadastroContaBancaria
+{
+    internal class CadastroConta
+    {
+        private int _conta { get ; set; }
+        private string _titular;
+        private char _opcao;
+       
+        private CadastroConta(int conta, string titular, char opcao) 
+        {
+            _conta = conta;
+            _titular = titular;
+            _opcao = opcao;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using ExercicioDeFixacao_CadastroContaBancaria;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("------- Banco Pimpão -------");
+
+        Console.WriteLine();
+        Console.Write("Entre com o número da conta (4 dígitos): ");
+        int conta = int.Parse(Console.ReadLine());
+        Console.Write("Entre com o nome do Titular da conta: ");
+        string titular = Console.ReadLine();
+        Console.Write("Haverá depósito inicial (s/n)? ");
+        char opcao = char.Parse(Console.ReadLine());
+
+        CadastroConta c = new CadastroConta(conta, titular, opcao);
+
+        Console.WriteLine();
+        Console.WriteLine(c);
+    }
+}
